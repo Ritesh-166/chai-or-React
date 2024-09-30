df748a4098a5eb2357c5909ee86f5b8675d86120
Ritesh kumar <riteshkumarrk166@gmail.com>
2024-09-30 23:38:48 +0530

    2nd video notes

--- 01BasicReact/src/main.jsx
+++ 01BasicReact/src/main.jsx
@@ -7,4 +7,18 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+  // react DOM create his own virtual dom and then compares with main DOM  and then do necessary changes in the main DOM
+  //React gives us power to render JSX (javascript ke through HTML ko render krne ke liye) . we are creating a kind of own tag like APP here
+// java script ke thruogh HTML likh paa rahe h oor this gives programming capbilities in HTML
+// yaha App.jsx me ek function h jo HTML return kr raha h oor main.jsx me render ho raha h . Index.html ke thruogh
+//here we can export only one element at a time.
+// to solve this issue we can enclose whoole in one div or
+// we can use fragment concept as shown next
+{/* <>
+<App/>
+<p>uihjbjd</p>
+</> */}
+
+//NOTE :- always captilize the name of the functions or components that we are going to define
+
 )
